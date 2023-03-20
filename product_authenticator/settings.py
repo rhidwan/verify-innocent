ca8dac5bbdf9b91884ab1a657365697eabae5b2c
Iqbal Mohammad Rhidwan <iqbalrhidwan52@gmail.com>
2023-03-20 08:23:32 +0600

    Updated settings.py

--- product_authenticator/settings.py
+++ product_authenticator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2)r!^hzfoldryu#%69c_z&l!u57gb6=^8-qjmht6^qqg=pt6(v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,9 +89,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'authenticator_prod',
-            'USER': 'u_authenticator',
-            'PASSWORD': 'AUTH_HARD_XYZ',
+            'NAME': '',
+            'USER': '',
+            'PASSWORD': '',
             'HOST': 'localhost',
             'PORT': '',
         }
